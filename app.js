17a4a06869c305762f359e89cfb0e03f887dde80
ycarr5150 <ycarrillo@csumb.edu>
2019-11-19 20:33:52 +0000

    adding sql connection

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express"); 
+const mysql = require('mysql');
 const app = express(); 
 app.engine('html', require('ejs').renderFile); 
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
+    const connection = mysql.createConnection({
+        host: 'mcldisu5ppkm29wf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'zzrbbsj5791xsnwf',
+        password: 'l4kg72cf660m8hya',
+        database: 'l2gh8fug1cqr96dc'
+    });
+    
     res.send("it works!"); 
 });
 
