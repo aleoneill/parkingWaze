e13577068833e5f7a0550a81ddeb11e67568c45f
Monica Ocampo <moocampo@csumb.edu>
2019-12-11 00:17:02 +0000

    small update

--- app.js
+++ app.js
@@ -78,20 +78,21 @@ app.get('/gmap', function(req, res, next) {
     }
 });
 
-// app.post('/gmap', function(req, res, next) {
-//     const connection = mysql.createConnection({
-//         host: 'mcldisu5ppkm29wf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//         user: 'zzrbbsj5791xsnwf',
-//         password: 'l4kg72cf660m8hya',
-//         database: 'l2gh8fug1cqr96dc'
-//     });
-//     connection.connect();
-//
-//     connection.query(
-//         `SELECT number FROM buildings
-//     WHERE number = '${req.body.buildingnum}'`
-//     )
-// });
+app.post('/gmap', function(req, res, next) {
+    const connection = mysql.createConnection({
+        host: 'mcldisu5ppkm29wf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'zzrbbsj5791xsnwf',
+        password: 'l4kg72cf660m8hya',
+        database: 'l2gh8fug1cqr96dc'
+    });
+    connection.connect();
+
+    connection.query(
+        `SELECT closest_lot, 2nd_closest, 3rd_closest FROM buildings
+         WHERE number = '${req.body.building}'`
+    
+        );
+    });
 
 app.get('/new', function(req, res) {
     res.render("new.html");
