aba886b050dbe00b01f8a577ac3a200ed8395361
Monica Ocampo <moocampo@csumb.edu>
2019-12-12 07:54:39 +0000

    fixing documentation

--- app.js
+++ app.js
@@ -294,7 +294,7 @@ app.listen("5000", "0.0.0.0", function() {
         console.log("Express Server is Running...");
 });
 
-// // // server listener - heroku ready
+//server listener - heroku ready
 // app.listen(process.env.PORT, process.env.IP, function() {
 //     console.log("Running Express Server...");
 // });
